582e8443eaedc4ccae99a2d04f03da0991a97ac3
Maria Laura Luraschi <loud.luraschi@gmail.com>
2023-10-20 15:38:13 +0200

    mimimum HTML

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
 import '../styles/layout/Header.scss';
 export const Header = () => {
+  const handleForm = (ev) => {
+    ev.preventDefault();
+  };
   return (
     <>
-        <h1 className='header__title'>Owen Wilson s wow</h1>
+      <h1 className='header__title'>Owen Wilson s wow</h1>
+      <form onSubmit={handleForm}>
+        <label>Movie
+          <input type='text' />
+        </label>
+        <label>Year
+          <input type='number' />
+        </label>
+      </form>
     </>
   );
 };
+export default Header;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,21 +3,22 @@ import '../styles/layout/Main.scss';
 export const Main = () => {
   return (
     <>
-      <ul></ul>
-
-      <li>
-        <article className='article'>
-          <img
-            src='#'
-            alt='poster'
-            className='article__poster'
-            placeholder='https://loremflickr.com/320/240/cinema'
-          />
-          <p className='article__movie'>movie</p>
-          <p className='article__year'>year</p>
-          <p className='article__quote'>frase</p>
-        </article>
-      </li>
+      <ul>
+        <li>
+          <article className='article'>
+            <img
+              src='#'
+              alt='poster'
+              className='article__poster'
+              placeholder='https://loremflickr.com/320/240/popcorn'
+            />
+            <p className='article__movie'>movie</p>
+            <p className='article__year'>year</p>
+            <p className='article__quote'>frase</p>
+          </article>
+        </li>
+      </ul>
     </>
   );
 };
+export default Main;
